edf936af61a6ecc9b94945ab899583d91197f6d4
Juha Määttänen <juha.maattanen@helsinki.fi>
2018-04-12 21:17:39 +0300

    Uusia testejä TextBuilder-luokkaan

--- TranscribeBuddy/src/test/java/jm/transcribebuddy/logics/TextBuilderTest.java
+++ TranscribeBuddy/src/test/java/jm/transcribebuddy/logics/TextBuilderTest.java
@@ -1,6 +1,5 @@
 package jm.transcribebuddy.logics;
 
-import jm.transcribebuddy.logics.TextBuilder;
 import org.junit.After;
 import org.junit.AfterClass;
 import org.junit.Before;
@@ -38,73 +37,67 @@ public class TextBuilderTest {
         assertEquals("", firstStatement);
     }
     
-    /**
-     * Test of getCurrent method, of class TextBuilder.
-     */
-//    @Test
-//    public void testGetCurrent() {
-//        System.out.println("getCurrent");
-//        TextBuilder instance = new TextBuilder();
-//        String expResult = "";
-//        String result = instance.getCurrent();
-//        assertEquals(expResult, result);
-//        // TODO review the generated test code and remove the default call to fail.
-//        fail("The test case is a prototype.");
-//    }
-
-    /**
-     * Test of getPrev method, of class TextBuilder.
-     */
-//    @Test
-//    public void testGetPrev() {
-//        System.out.println("getPrev");
-//        TextBuilder instance = new TextBuilder();
-//        String expResult = "";
-//        String result = instance.getPrev();
-//        assertEquals(expResult, result);
-//        // TODO review the generated test code and remove the default call to fail.
-//        fail("The test case is a prototype.");
-//    }
-
-    /**
-     * Test of getNext method, of class TextBuilder.
-     */
-//    @Test
-//    public void testGetNext() {
-//        System.out.println("getNext");
-//        TextBuilder instance = new TextBuilder();
-//        String expResult = "";
-//        String result = instance.getNext();
-//        assertEquals(expResult, result);
-//        // TODO review the generated test code and remove the default call to fail.
-//        fail("The test case is a prototype.");
-//    }
-
-    /**
-     * Test of getAll method, of class TextBuilder.
-     */
-//    @Test
-//    public void testGetAll() {
-//        System.out.println("getAll");
-//        TextBuilder instance = new TextBuilder();
-//        String expResult = "";
-//        String result = instance.getAll();
-//        assertEquals(expResult, result);
-//        // TODO review the generated test code and remove the default call to fail.
-//        fail("The test case is a prototype.");
-//    }
-
-    /**
-     * Test of set method, of class TextBuilder.
-     */
-//    @Test
-//    public void testSet() {
-//        System.out.println("set");
-//        String statement = "";
-//        TextBuilder instance = new TextBuilder();
-//        instance.set(statement);
-//        // TODO review the generated test code and remove the default call to fail.
-//        fail("The test case is a prototype.");
-//    }
+    @Test
+    public void getPrevNotifiesOfTheBeginning1() {
+        String prev = textBuilder.getPrev();
+        assertEquals("*Projektin alku*", prev);
+    }
     
+    @Test
+    public void getPrevNotifiesOfTheBeginning2() {
+        final String statement = "Testi lause.";
+        final int additions = 10;
+        for(int i = 0; i < additions; i++)
+            textBuilder.endStatement(statement);
+        for(int i = 0; i < additions; i++)
+            textBuilder.selectPrev();
+        // now we should be working with the first statement
+        String prev = textBuilder.getPrev();
+        assertEquals("*Projektin alku*", prev);
+    }
+    
+    @Test
+    public void getNextNotifiesOfTheEnd() {
+        final String statement = "Testi lause.";
+        final int additions = 10;
+        for(int i = 0; i < additions; i++)
+            textBuilder.endStatement(statement);
+        // now we should be working with the last statement
+        String next = textBuilder.getNext();
+        assertEquals("*Projektin loppu*", next);
+    }
+    
+    @Test
+    public void getAllBuildsTextRight() {
+        textBuilder.endStatement("Yks.");
+        textBuilder.endStatement("Kaks.");
+        textBuilder.set("Kolme.");
+        String text = textBuilder.getAll();
+        assertEquals("Yks. Kaks. Kolme.", text);
+    }
+    
+    @Test
+    public void parseFromAllRecognizesAdditions() {
+        textBuilder.endStatement("Eka.");
+        textBuilder.endStatement("Toka.");
+        String text = textBuilder.getAll();
+        text = text + "Uutta tekstiä.";
+        // text should be now "Eka. Toka. Uutta tekstiä."
+        textBuilder.parseFromAll(text);
+        String statement = textBuilder.getCurrent();
+        assertEquals("Uutta tekstiä.", statement);
+    }
+    
+    @Test
+    public void parseFromAllRecognizesModifications() {
+        textBuilder.endStatement("Eka.");
+        textBuilder.endStatement("Toka.");
+        textBuilder.set("Neljäs.");
+        String text = textBuilder.getAll();
+        // text should be now "Eka. Toka. Neljäs."
+        text = text.substring(0, 11) + "Kolmas.";
+        textBuilder.parseFromAll(text);
+        String statement = textBuilder.getCurrent();
+        assertEquals("Kolmas.", statement);
+    }
 }
